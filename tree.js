ab6cae5be76c8d815edcde26cc628ece0035b238
Uri Goldshtein <uri.goldshtein@gmail.com>
2013-02-06 22:58:22 +0200

    Beauty

--- tree.js
+++ tree.js
@@ -800,9 +800,9 @@ function init(){
                 data: {'url':'#/?urlParam=Health_and_beauty/Longevity'},
                 children: []
             }, {
-                id: "Health_and_beauty/Longevity",
-                name: "Longevity",
-                data: {'url':'#/?urlParam=Health_and_beauty/Longevity'},
+                id: "Health_and_beauty/Beauty",
+                name: "Beauty",
+                data: {'url':'#/?urlParam=Health_and_beauty/Beauty'},
                 children: []
             },{
                 id: "Health_and_beauty/Fitness",
